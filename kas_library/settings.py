0a571f560d5beed0edbbf05ed484d734326e1e09
Artem Kuznetsov <artem.kuznetsov@osinit.com>
2021-09-27 11:53:44 +0300

    Remove secret_key

--- kas_library/settings.py
+++ kas_library/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a@^tqg_32%3%jl3q&hfbcbdo@n3d=-+jtfha&&dq_fw^v6f@9u'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
